e43bd7daded63501373d75b6991798f2dcbc7539
Sweta Kyada <swetakyada20@gmail.com>
2022-02-23 00:59:46 +0530

    Repair

--- backend/routes/doctor.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createDoctor, loginDoctor } from "../controllers/doctor.js";
-
-const router = express.Router();
-
-router.post("/register", createDoctor);
-router.post("/login", loginDoctor);
-
-export default router;
